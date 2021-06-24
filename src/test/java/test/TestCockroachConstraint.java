07e10c93559f4cf146e055db0effcd3a98ce7ff9
David Phillips <david@acz.org>
2021-06-23 17:22:04 -0700

    Add tests for error detail

--- src/test/java/test/TestCockroachConstraint.java
+++ src/test/java/test/TestCockroachConstraint.java
@@ -20,38 +20,55 @@ public class TestCockroachConstraint
     public void testConstraintPostgreSql()
             throws SQLException
     {
-        testConstraint(new PostgreSQLContainer<>("postgres:13"));
+        testConstraint(new PostgreSQLContainer<>("postgres:13"), false);
     }
 
     @Test
     public void testConstraintCockroach()
             throws SQLException
     {
-        testConstraint(new CockroachContainer("cockroachdb/cockroach:v21.1.3"));
+        testConstraint(new CockroachContainer("cockroachdb/cockroach:v21.1.3"), true);
     }
 
-    private static void testConstraint(JdbcDatabaseContainer<?> database)
+    private static void testConstraint(JdbcDatabaseContainer<?> database, boolean cockroach)
             throws SQLException
     {
         database.start();
 
         try (Connection connection = DriverManager.getConnection(database.getJdbcUrl(), database.getUsername(), database.getPassword())) {
             try (Statement statement = connection.createStatement()) {
-                statement.execute("CREATE TABLE items (item_id INT PRIMARY KEY, name TEXT UNIQUE)");
-                statement.execute("INSERT INTO items VALUES (123, 'Apple')");
-                statement.execute("INSERT INTO items VALUES (456, 'Orange')");
+                statement.execute("CREATE TABLE items (item_id INT PRIMARY KEY, name TEXT UNIQUE, a INT, b INT, UNIQUE (a, b))");
+                statement.execute("INSERT INTO items VALUES (123, 'Apple', 1, 2)");
+                statement.execute("INSERT INTO items VALUES (456, 'Orange', 3, 4)");
             }
 
             try (Statement statement = connection.createStatement()) {
                 assertThatThrownBy(() -> statement.execute("INSERT INTO items VALUES (123, 'Grape')"))
-                        .isInstanceOfSatisfying(PSQLException.class, e ->
-                                assertThat(e.getServerErrorMessage().getConstraint()).isIn("primary", "items_pkey"));
+                        .isInstanceOfSatisfying(PSQLException.class, e -> {
+                            assertThat(e.getServerErrorMessage()).isNotNull();
+                            assertThat(e.getServerErrorMessage().getConstraint()).isEqualTo(cockroach ? "primary" : "items_pkey");
+                            assertThat(e.getServerErrorMessage().getDetail()).isEqualTo("Key (item_id)=(123) already exists.");
+                        });
             }
 
             try (Statement statement = connection.createStatement()) {
                 assertThatThrownBy(() -> statement.execute("INSERT INTO items VALUES (789, 'Apple')"))
-                        .isInstanceOfSatisfying(PSQLException.class, e ->
-                                assertThat(e.getServerErrorMessage().getConstraint()).isEqualTo("items_name_key"));
+                        .isInstanceOfSatisfying(PSQLException.class, e -> {
+                            assertThat(e.getServerErrorMessage()).isNotNull();
+                            assertThat(e.getServerErrorMessage().getConstraint()).isEqualTo("items_name_key");
+                            assertThat(e.getServerErrorMessage().getDetail()).isEqualTo(
+                                    cockroach ? "Key (name)=('Apple') already exists." : "Key (name)=(Apple) already exists.");
+                        });
+            }
+
+            try (Statement statement = connection.createStatement()) {
+                assertThatThrownBy(() -> statement.execute("INSERT INTO items VALUES (789, 'Grape', 3, 4)"))
+                        .isInstanceOfSatisfying(PSQLException.class, e -> {
+                            assertThat(e.getServerErrorMessage()).isNotNull();
+                            assertThat(e.getServerErrorMessage().getConstraint()).isEqualTo("items_a_b_key");
+                            assertThat(e.getServerErrorMessage().getDetail()).isEqualTo(
+                                    cockroach ? "Key (a,b)=(3,4) already exists." : "Key (a, b)=(3, 4) already exists.");
+                        });
             }
         }
     }
